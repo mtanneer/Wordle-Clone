2bb716c212ee39d634d80ef61d8c9780b2ba6404
Manas Tanneeru <tmanas9@outlook.com>
2024-07-05 18:20:28 -0700

    Added Debug Mode, cleaned up

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from wordlist import *
-from PyDictionary import PyDictionary
 import math,string,random,time,sys
-
-dictionary=PyDictionary()
 
 if __name__ == '__main__' :
     count = 0
     ans = random.choice(word_list)
+    
+    # DEBUG MODE
+    if '-d' in sys.argv:
+        print(ans)
+    
     list_ans = list(ans)
     print("Enter a 5 letter word:")
     while(1):        
         if(count == 6):
             print(u"Out of tries:( But the word was\u001b[32m %s\u001b[0m\n " % ans)
-            # print(dictionary.meaning(ans))
             break
         
         word = input()
         list_in = list(word)
         sys.stdout.write(u"\u001b[A")
-        # if(list_in [0]== "-"):
-        #     print(dictionary.meaning(ans))
-        #     time.sleep(3.5)
-        #     print ("\033[A                             \033[A")
-        #     continue
+
         if(len(word) == 5):
             if(word not in word_list):
                 print(word +" is not valid")
@@ -43,18 +40,16 @@ if __name__ == '__main__' :
             if(word == ans):
                 count += 1
                 print("Congrats\n")
-                # print(dictionary.meaning(ans))
                 break
             else:
                 count += 1
                 
-
         else:
             print("Word needs to exactly 5 letters long")
             time.sleep(1.5)
             print ("\033[A                                                    \033[A")
             continue
-        #count2 += 1
+        
         
         
         
