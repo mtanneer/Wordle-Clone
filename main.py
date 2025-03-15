c6d444adb2c8009d2c910ba4c219e04df941260d
Manas Tanneeru <tmanas9@outlook.com>
2025-03-15 00:44:17 -0700

    pycharm

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if __name__ == '__main__' :
     
     list_ans = list(ans)
     print("Enter a 5 letter word:")
-    while(1):        
-        if(count == 6):
+    while 1:
+        if count == 6:
             print(u"Out of tries:( But the word was\u001b[32m %s\u001b[0m\n " % ans)
             break
         
@@ -20,24 +20,24 @@ if __name__ == '__main__' :
         list_in = list(word)
         sys.stdout.write(u"\u001b[A")
 
-        if(len(word) == 5):
-            if(word not in word_list):
+        if len(word) == 5:
+            if word not in word_list:
                 print(word +" is not valid")
                 time.sleep(1.5)
                 print ("\033[A                             \033[A")
                 continue  
 
             for i in range(len(list_in)):
-                if(list_in[i] == list_ans[i]):
+                if list_in[i] == list_ans[i]:
                     print(u"\u001b[32m %s\u001b[0m" % list_in[i],end='')
-                elif(list_in[i] in list_ans):
+                elif list_in[i] in list_ans:
                     print(u"\u001b[33m %s\u001b[0m" % list_in[i],end='')
                 else:
                     print(" %s" % list_in[i],end='')
 
             print("",end='\n')         
             
-            if(word == ans):
+            if word == ans:
                 count += 1
                 print("Congrats\n")
                 break
